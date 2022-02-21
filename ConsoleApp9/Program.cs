6dd495d78b39a56f04bb7dc4b69883ddeffa036f
ankitsinghrana13 <ak9897432538@gmail.com>
2022-02-21 17:28:34 +0800

    Add project files.

---
+++ ConsoleApp9/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace ConsoleApp9
+{
+   class emply
+    {
+        public int a;
+        public int b;
+
+        public void data(int a,int b)
+        {
+            this.a = a;
+            this.b = b;
+            Console.WriteLine("A=" + a);
+            Console.WriteLine("B=" + b);
+            Console.WriteLine(a + b);
+
+        }
+    }
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            emply obj = new emply();
+            obj.data(10, 20);
+        }
+    }
+}
